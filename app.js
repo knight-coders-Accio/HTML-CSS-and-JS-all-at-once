131e7e35da3f8d49ca19741d75efb873c398c2fb
Abhishek <theabhishek.srm@gmail.com>
2024-06-20 22:33:26 +0530

    query selecter

---
+++ app.js
@@ -0,0 +1,22 @@
+
+// selection of the elements
+
+// querySelector and querySelectorAll
+
+// querySelector with tag name 
+// const heading = document.querySelector('h1');
+
+// querySelector with class name
+// const heading = document.querySelector('.title');
+
+// querySelector with id
+// const heading = document.querySelector('#title');
+
+
+// querySelectorAll
+// const heading = document.querySelectorAll('.title');
+
+
+
+
+// innerText, innerHTML
